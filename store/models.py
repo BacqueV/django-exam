882c2b7037db54dbdea5f226cdea0d5e828cdbcd
BacqueV <ahmedovbahtiar55@gmail.com>
2023-03-25 13:58:56 +0500

    bug fix

--- store/models.py
+++ store/models.py
@@ -6,6 +6,10 @@ class Category(models.Model):
     title = models.CharField(max_length=60)
     slug = models.SlugField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.title
 

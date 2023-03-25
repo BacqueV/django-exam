73c4e8f008423d875ee3f765f8d1c90335e23b5a
BacqueV <ahmedovbahtiar55@gmail.com>
2023-03-25 13:38:03 +0500

    first commit

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register(r'categories', views.CategoryViewSet, basename='category')
-router.register(r'boilers', views.ProductViewSet, basename='boiler')
+router.register(r'product', views.ProductViewSet, basename='product')
 router.register(r'orders', views.OrderViewSet, basename='order')
 router.register(r'order_items', views.OrderItemViewSet, basename='order_items')
 
